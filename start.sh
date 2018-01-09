cc97d6de19026e342b1a089dce711289a98baca2
longshengwang <wanglongshengdf@126.com>
2018-01-09 09:41:08 +0800

    输出重定向

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "1. 启动前台"
 cd website
-node app.js&
+node app.js>/dev/null 2>&1 &
 
 echo "2. 启动后台"
 cd ../webserver/spy
-node index.js&
+node index.js>/dev/null 2>&1 &
 
 echo "==>启动完毕!"
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps aux|grep node |grep -v grep |awk '{print $2}'|kill -9
+ps aux|grep node |grep -v grep |awk '{print $2}'|xargs kill -9
 
